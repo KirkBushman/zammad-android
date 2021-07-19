003259187e1b2fba10c4b1d27dc561aa015cb1d1
Ese Udom <udomese@yahoo.com>
2021-07-19 01:25:14 +0100

    Support other authentication methods apart from username and password

---
+++ lib/src/main/java/com/kirkbushman/zammad/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.kirkbushman.zammad.utils
+
+object Constants {
+    const val AUTH_TYPE_USERNAME_PASSWORD = 0
+    const val AUTH_TYPE_ACCESS_TOKEN = 1
+    const val AUTH_TYPE_OAUTH2 = 2
+}
